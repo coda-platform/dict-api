f6ca1660349f9e04914f03a65d137067c01a4798
Louis Mullie <louis.mullie@gmail.com>
2022-10-14 23:46:53 -0400

    Initial import

---
+++ index.js
@@ -0,0 +1,84 @@
+const fs = require('fs')
+const Airtable = require('./Airtable')
+
+const siteNames = ['CHUM', 'CISSSCA', 'CHUQ','CIUSSSNIM']
+
+class Fetcher extends Airtable {
+  constructor(...args) {
+    super(...args)
+    this.tableSettings = [
+      ["PCRName", {}],
+      ["PCRResultStatus", {}],
+      ["PCRSite", {}],
+      ["LabSite", {}],
+      ["LabName", {}],
+      ["ObservationNameUnit", {}],
+      ["CultureName", {}],
+      ["CultureResultStatus", {}],
+      ["CultureSite", {}],
+      ["DrugName", {}],
+      ["DrugRoute", {}],
+      ["DrugCodes", {}],
+      ["DrugFrequency", {}],
+      ["UnitType", {}],
+      ["ImagingName", {}],
+      ["ImagingSite", {}],
+      ["ProcedureName", {}],
+      ["Priority", {}],
+      ["ServiceType", {}],
+      ["VitalSignName", {}],
+      ["ProcCancelReason", {}]
+    ]
+  }
+}
+
+const getSiteMappings = async (siteName) => {
+
+  const fetcher = new Fetcher({ 
+    baseId: 'appKxBkg0yI3NVqkz', 
+    apiKey: 'keyggEJjl4atn9Mkd'
+  })
+
+  const tables = await fetcher.fetchBase()
+  const filteredTables = {}
+  
+  for (let tableName in tables) {
+    let table = tables[tableName]
+    
+    filteredTables[tableName] = table.filter( (rec) => 
+      (rec.fields.site && ( rec.fields.site.includes(siteName) || rec.fields.site.includes('ALL') )  ))
+  }
+  
+  const mappedTables = {}
+  
+  for (let tableName in filteredTables) {
+    let table = filteredTables[tableName]
+    mappedTables[tableName] = table.map((row) => {
+      let mappedRow = {}
+      for (let field in row.fields) {
+        if (field == 'raw_string_lower') {
+          mappedRow[field] = row.fields[field].toLowerCase()
+        } else if (field == 'site') {
+          mappedRow[field] = siteName
+        } else {
+          mappedRow[field] = row.fields[field]
+        } 
+      }
+      return mappedRow
+    })
+  }
+  
+  return mappedTables
+  
+}
+
+const getAllMappings = async (siteName) => {
+  for (let siteName of siteNames) {
+    const siteJson = await getSiteMappings(siteName)
+    fs.writeFileSync(`./dictionaries/${siteName}.json`, 
+      JSON.stringify(siteJson, null, 2))
+  }
+}
+
+getAllMappings()
+
